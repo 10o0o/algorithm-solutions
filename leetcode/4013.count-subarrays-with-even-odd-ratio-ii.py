100013eabce1417fb94146acdf5efed211a30fe0
10o0o <wotjs2923@gmail.com>
2026-08-06 18:02:34 +0900

    LC(contest, hard): 4013 - 좌표압축, 펜윅

--- leetcode/4013.count-subarrays-with-even-odd-ratio-ii.py
+++ leetcode/4013.count-subarrays-with-even-odd-ratio-ii.py
@@ -5,9 +5,58 @@
 #
 
 # @lc code=start
+
+
+class FenwickTree:
+    def __init__(self, size):
+        self.tree = [0] * (size + 1)
+
+    def add(self, index, value=1):
+        while index < len(self.tree):
+            self.tree[index] += value
+            index += index & -index
+
+    def prefix_sum(self, index):
+        result = 0
+
+        while index > 0:
+            result += self.tree[index]
+            index -= index & -index
+
+        return result
+
+
 class Solution:
     def countRatioSubarrays(self, nums: list[int], a: int, b: int) -> int:
-        return 0
+        scores = [0]
+        current_score = 0
+
+        for num in nums:
+            if num % 2 == 0:
+                current_score += b
+            else:
+                current_score -= a
+
+            scores.append(current_score)
+
+        sorted_scores = sorted(set(scores))
+        rank = {value: index + 1 for index, value in enumerate(sorted_scores)}
+        fenwick = FenwickTree(len(sorted_scores))
+
+        answer = 0
+        seen_count = 0
+
+        for score in scores:
+            current_rank = rank[score]
+
+            less_count = fenwick.prefix_sum(current_rank - 1)
+            greater_equal_count = seen_count - less_count
+            answer += greater_equal_count
+
+            fenwick.add(current_rank)
+            seen_count += 1
+
+        return answer
 
 
 # @lc code=end
