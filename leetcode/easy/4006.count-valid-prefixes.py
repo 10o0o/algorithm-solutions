2789621a49dff08c93ed114c88d117ee2cfadfe6
10o0o <wotjs2923@gmail.com>
2026-08-07 09:00:16 +0900

    chore(leetcode): organize problems by difficulty

---
+++ leetcode/easy/4006.count-valid-prefixes.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=4006 lang=python3
+#
+# [4006] Count Valid Prefixes
+#
+
+# @lc code=start
+class Solution:
+    def countValidPrefixes(self, s: str) -> int:
+
+# @lc code=end
